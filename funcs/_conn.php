9f994c79b684712bab21fcd77a9f366370355354
Borja Live <borjainlive@gmail.com>
2020-06-06 17:34:09 +0200

    Remodelado

--- funcs/_conn.php
+++ funcs/_conn.php
@@ -4,7 +4,7 @@ include "__mail.php";
 
 $sql_host = "localhost";
 $sql_user = "root";
-$sql_pass = "";
+$sql_pass = "Spike4419";
 $sql_base = "registro";
 
 $nombre_meses = array('01' => 'Enero', '02' => 'Febrero', '03' => 'Marzo', '04' => 'Abril', '05' => 'Mayo', '06' => 'Junio', '07' => 'Julio', '08' => 'Agosto', '09' => 'Septiembre', '10' => 'Octubre', '11' => 'Noviembre', '12' => 'Diciembre');
